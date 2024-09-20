357332a8cb5a842f67a554c0ea7d8d787f3ef2ea
Ananda Ravi Kuntadi <anandaravik@gmail.com>
2024-09-20 16:19:41 +0700

    Create WelcomeScreenPreview.kt

---
+++ app/src/main/java/com/tifd/projectcomposed/WelcomeScreenPreview.kt
@@ -0,0 +1,13 @@
+package com.tifd.projectcomposed
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.tifd.projectcomposed.ui.theme.ProjectComposeDTheme
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ProjectComposeDTheme {
+        WelcomeScreen()
+    }
+}
