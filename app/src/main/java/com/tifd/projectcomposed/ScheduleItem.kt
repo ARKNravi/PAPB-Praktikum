236f7543660a5151ca28d4fc5294a9a702395f8b
Ananda Ravi Kuntadi <anandaravik@gmail.com>
2024-09-28 22:23:22 +0700

    Create ScheduleItem.kt

---
+++ app/src/main/java/com/tifd/projectcomposed/ScheduleItem.kt
@@ -0,0 +1,12 @@
+package com.tifd.projectcomposed
+
+data class ScheduleItem(
+    val hari: String = "",
+    val jamMulai: String = "",
+    val jamSelesai: String = "",
+    val kelas: String = "",
+    val kode: String = "",
+    val mataKuliah: String = "",
+    val dosen: String = "",
+    val ruang: String = ""
+)
